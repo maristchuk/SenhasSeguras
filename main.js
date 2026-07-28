41209296510d373d922d9219f36f17b5c5e4963f
maristchuk <mariana.istchuk@escola.pr.gov.br>
2026-07-28 17:09:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -23,12 +23,20 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?$#&';
 gerarSenha();
 
 
 function gerarSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMa
+    }
     let senha = '';
     for (let i = 0; i <tamanhoSenha;i++){    
     let numeroAleatorio =Math.random()*letrasMaiusculas.length;
