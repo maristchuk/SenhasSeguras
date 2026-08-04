f8c649a768f263c652668a65685bb74bb96094f7
maristchuk <mariana.istchuk@escola.pr.gov.br>
2026-08-04 16:22:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,54 +12,51 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-    gerarSenha();
+    geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
-    gerarSenha();
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-for (i=0, i < checkbox.length;i++){
-    checkbox[i].onclick = gerarSenha;
+for (i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
 }
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
-const simbolos = '!@%*?$#&';
-gerarSenha();
+const simbolos = '!@#$%&*?';
+
+geraSenha();
 
 
-function gerarSenha(){
+function geraSenha(){
     let alfabeto = '';
-    if (checkbox[0].checked){
+    if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
-
-    if (checkbox[1].checked){
+    if (checkbox[1].checked) {
         alfabeto = alfabeto + letrasMinusculas;
     }
-
-    if (checkbox[2].checked){
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
-    }    
-    
-    if (checkbox[3].checked){
-            alfabeto = alfabeto + numeros;
     }
 
     let senha = '';
-    for (let i = 0; i <tamanhoSenha;i++){   
-    let numeroAleatorio =Math.random()*alfabeto.length;
+    for (let i = 0; i < tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*alfabeto.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
     senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
-
